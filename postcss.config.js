0f4882cc87532f3321d4a4eca912a4df587b4d79
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-19 06:10:27 -0500

    updates

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+// const purgecss = [
+// 	'@fullhuman/postcss-purgecss',
+// 	{
+// 		content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+// 		defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+// 	}
+// ];
+
+module.exports = {
+	plugins: [
+		'tailwindcss',
+		'autoprefixer',
+		'postcss-flexbugs-fixes',
+		// ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+		[
+			'postcss-preset-env',
+			{
+				autoprefixer: {
+					flexbox: 'no-2009'
+				},
+				stage: 3,
+				features: {
+					'custom-properties': false
+				}
+			}
+		]
+	]
+};
+
+// https://dev.to/joserfelix/how-to-make-a-static-blog-with-next-js-2bd6
