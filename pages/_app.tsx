6f528956be22fd3fd8a874a810fca9cf9056f776
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-22 06:21:48 -0500

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,30 @@ import '@tailwindcss/typography';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { AppProps, NextWebVitalsMetric } from 'next/app';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from 'lib/google-analytics';
+import ReactGA from 'react-ga';
+
+export const gaInit = () => {
+	ReactGA.initialize(`UA-${process.env.GA_TRACKING_ID}`);
+};
 
 config.autoAddCss = false;
 
 function App({ Component, pageProps }: AppProps): ReactElement {
+	const router = useRouter();
+	const { logPageView } = gtag;
+	useEffect(() => {
+		const handleRouteChange = () => {
+			logPageView();
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
 	return <Component {...pageProps} />;
 }
 
@@ -16,3 +35,23 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
 }
 
 export default App;
+
+// Configuring Fortawesome with nextjs v9+
+// https://github.com/UnlyEd/next-right-now/blob/master/src/pages/_app.tsx
+// https://stackoverflow.com/questions/44752189/how-to-add-font-awesome-to-next-js-project
+
+/*
+	const router = useRouter();
+	const { pageview } = gtag;
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, []);
+	return <Component {...pageProps} />;
+}
+*/
