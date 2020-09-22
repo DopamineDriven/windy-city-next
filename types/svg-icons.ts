7f45720174802f5b71289620ba6b34d4759b31a7
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-22 05:24:09 -0500

    building it out

---
+++ types/svg-icons.ts
@@ -0,0 +1,37 @@
+interface SvgIconConstituentValues {
+	strokeColor?: string[];
+	strokeWidth?: string[];
+	strokeFill?: string[];
+	fillColor?: string[];
+	imageWidth?: string;
+	imageHeight?: string;
+	width?: string;
+	height?: string;
+	rotateCenter?: number;
+	classNames?: string[];
+}
+
+export default SvgIconConstituentValues;
+
+// Variadic Tuple Types
+// https://devblogs.microsoft.com/typescript/announcing-typescript-4-0/
+
+/*
+interface SvgIconConstituentValues {
+	strokeColor?: string[];
+	strokeWidth?: string[];
+	strokeFill?: string[];
+	fillColor?: string[];
+	imageWidth?: string;
+	imageHeight?: string;
+	width?: string;
+	height?: string;
+	rotateCenter?: number;
+	className?: string[];
+}
+
+export default SvgIconConstituentValues;
+
+// Variadic Tuple Types
+// https://devblogs.microsoft.com/typescript/announcing-typescript-4-0/
+*/
