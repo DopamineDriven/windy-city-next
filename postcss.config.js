de66e50289ef9b66ded9b42edbcaa7c4c9f89cf7
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-23 12:16:56 -0500

    meta-data configured

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,16 @@
 const purgecss = [
 	'@fullhuman/postcss-purgecss',
 	{
-		// Specify the paths to all of the template files
-		content: ['./pages/**/*.tsx', './components/**/*.tsx'],
-		// function to extract class names from the templates
-		defaultExtractor: content => {
-			// Capture as liberally as possible, including things like `h-(screen-1.5)`
-			const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-			// Capture classes within other delimiters like .block(class="w-1/2") in Pug
-			const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-			return broadMatches.concat(innerMatches);
-		}
+		content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+		defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 	}
 ];
-
 module.exports = {
 	plugins: [
 		'tailwindcss',
 		'autoprefixer',
 		'postcss-flexbugs-fixes',
-		...(process.env.NODE_ENV === 'production' ? purgecss : undefined),
+		...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
 		[
 			'postcss-preset-env',
 			{
