099962ac1d2d61b2a2282bbbef5a100d489f87db
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-23 13:26:04 -0500

    meta-data configured

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
-const purgecss = [
-	'@fullhuman/postcss-purgecss',
-	{
-		content: ['./components/**/*.tsx', './pages/**/*.tsx'],
-		defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-	}
-];
+// const purgecss = [
+// 	'@fullhuman/postcss-purgecss',
+// 	{
+// 		content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+// 		defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+// 	}
+// ];
 module.exports = {
 	plugins: [
 		'tailwindcss',
 		'autoprefixer',
-		'postcss-flexbugs-fixes',
-		...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+		'postcss-flexbugs-fixes'
+		// ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
 		[
 			'postcss-preset-env',
 			{
