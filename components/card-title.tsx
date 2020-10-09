82fceff81aa98509aff719a8a21147fb6686469c
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-08 23:56:41 -0500

    troubleshooting success

--- components/card-title.tsx
+++ components/card-title.tsx
@@ -9,7 +9,7 @@ interface CardTitleProps {
 
 const CardTitle = ({ title, slug }: CardTitleProps): JSX.Element => {
 	return (
-		<div className='block w-auto font-bold leading-tight text-customCardTitle pl-portfolioDivider h-paddingPostTitleTop sm:h-paddingPostTitleBottom'>
+		<div className='block w-auto font-bold leading-tight text-customCardTitle pl-portfolioDivider h-paddingPostTitleTop sm:h-paddingPostTitleBottom lg:h-portfolio'>
 			<Link as={`/posts/${slug}`} href='/posts/[slug]' passHref scroll={true}>
 				<a
 					className='block font-semibold text-left transition-all duration-1000 transform animate-hero font-head hover:text-tertiary translate-y-portfolioDivider'
