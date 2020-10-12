a555fcbbdd45e6a3ad40696253779afefd173b86
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-12 17:56:45 -0500

    codegen continued

--- components/card-filter.tsx
+++ components/card-filter.tsx
@@ -15,7 +15,7 @@ interface CardFilterProps {
 const { ASC, DESC } = OrderEnum;
 const { AUTHOR, DATE, MODIFIED, TITLE } = PostObjectsConnectionOrderbyEnum;
 
-const Dropdown = ({ filter, setFilter }: CardFilterProps) => {
+const CardFilter = ({ filter, setFilter }: CardFilterProps) => {
 	const [active, setActive] = useState(false);
 
 	const buttonClasses = `inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-blue-500 active:text-gray-200 transition ease-in-out duration-150`;
@@ -55,3 +55,5 @@ const Dropdown = ({ filter, setFilter }: CardFilterProps) => {
 		></div>
 	);
 };
+
+export default CardFilter;
