6525e49a9a9575e4596a67951e4c771033e1870e
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-12 03:49:10 -0500

    building out schema

--- graphql/api-post-slugs.ts
+++ graphql/api-post-slugs.ts
@@ -1,25 +1,4 @@
 import { gql } from '@apollo/client';
-import { fetchAPI } from 'lib/api';
-import { getAllPostsWithSlug } from 'graphql/__generated__/getAllPostsWithSlug';
-
-// const GET_ALL_POSTS_WITH_SLUG = async () => {
-// 	const postSlugs = await fetchAPI(
-// 		gql`
-// 			query getAllPostsWithSlug {
-// 				posts(first: 10000) {
-// 					edges {
-// 						node {
-// 							slug
-// 						}
-// 					}
-// 				}
-// 			}
-// 		`
-// 	);
-// 	return postSlugs?.posts;
-// };
-
-// export default GET_ALL_POSTS_WITH_SLUG;
 
 const POST_SLUGS = gql`
 	query getAllPostsWithSlug {
