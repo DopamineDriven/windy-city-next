478f22e63e14f86bcae27ca93187877422a2559c
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-12 17:59:05 -0500

    codegen continued

--- components/card-filter.tsx
+++ components/card-filter.tsx
@@ -35,11 +35,21 @@ const CardFilter = ({ filter, setFilter }: CardFilterProps) => {
 				value={filter}
 				onChange={() => setFilter(filter)}
 				className='block appearance-none w-full bg-primary text-primary border border-primary px-portfolioRS py-portfolioDivider leading-headerAbout focus:outline-none'
-			></select>
-			<option value={AUTHOR}>Author</option>
-			<option value={DATE}>Date</option>
-			<option value={MODIFIED}>Modified</option>
-			<option value={TITLE}>Title</option>
+			>
+				<option value={AUTHOR}>Author</option>
+				<option value={DATE}>Date</option>
+				<option value={MODIFIED}>Modified</option>
+				<option value={TITLE}>Title</option>
+			</select>
+			<div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+				<svg
+					className='fill-current h-4 w-4'
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 20 20'
+				>
+					<path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+				</svg>
+			</div>
 		</div>
 	);
 
