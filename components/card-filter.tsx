1c20413137ab6f4cf60c21df464ce15e8ebd440f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-12 17:39:35 -0500

    codegen continued

--- components/card-filter.tsx
+++ components/card-filter.tsx
@@ -3,7 +3,7 @@ import {
 	OrderEnum
 } from '../types/graphql-global-types';
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 interface CardFilterProps {
 	filter: PostObjectsConnectionOrderbyEnum;
@@ -19,4 +19,31 @@ const Dropdown = ({ filter, setFilter }: CardFilterProps) => {
 	const [active, setActive] = useState(false);
 
 	const buttonClasses = `inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-blue-500 active:text-gray-200 transition ease-in-out duration-150`;
+
+	const buttonDrop = (
+		<button
+			onClick={active => setActive(!active)}
+			className={` ${buttonClasses}`}
+		>
+			Sort By
+		</button>
+	);
+
+	const OptionConfig = () => (
+		<div className='inline-block w-portfolioLS'>
+			<select className='block appearance-none w-full bg-primary text-primary border border-primary px-portfolioRS py-portfolioDivider leading-headerAbout focus:outline-none'></select>
+		</div>
+	);
+
+	const DivConditional = () => (
+		<div
+			className={classNames(
+				'origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg',
+				{
+					block: active,
+					hidden: !active
+				}
+			)}
+		></div>
+	);
 };
